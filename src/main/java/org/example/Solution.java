1028209fcfd23b481f07c8e73de3e5c142619f7d
Alvin <alvinngg9@gmail.com>
2024-08-22 18:24:14 -0700

    Update Solution.java

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Solution {
     public void rotate(int[][] matrix) {
+        //transpose the array
         for (int i = 0; i < matrix.length; i++) {
             for (int j = i; j < matrix[i].length; j++) {
                 int temp = matrix[i][j];
@@ -9,8 +10,10 @@ public class Solution {
                 matrix[j][i] = temp;
             }
         }
+
+        //swap all values in each row
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length / 2; j++) {
+            for (int j = 0; j < matrix.length/2; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[i][matrix.length - 1 - j];
                 matrix[i][matrix.length - 1 - j] = temp;
